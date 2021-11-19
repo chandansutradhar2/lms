cb9b8eb5d7fc4e82527913282e05faccc17d8b33
chandan_naresh <chandansutradhar2@gmail.com>
2021-11-19 12:59:17 +0530

    routing with routerLink demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { MyprofileComponent } from './myprofile/myprofile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'profile', component: MyprofileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { UikitModule } from './uikit/uikit.module';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { CoursesComponent } from './courses/courses.component';
+import { MyprofileComponent } from './myprofile/myprofile.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     SidemenuComponent,
+    HomeComponent,
+    AboutusComponent,
+    CoursesComponent,
+    MyprofileComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, UikitModule],
   providers: [],
